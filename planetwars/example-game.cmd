a39810e3f5a9154a08605abd74e6d1c51683f1a0
UncleVasya (Oleg Ovcharenko) <Oleg_Ovcharenko@list.ru>
2014-06-12 09:48:45 +0300

    #2 use --strict option

--- planetwars/example-game.cmd
+++ planetwars/example-game.cmd
@@ -1,2 +1,2 @@
-python playgame.py --verbose --fill --log_input --log_output --log_error --log_dir game_logs --turns 80 --map_file maps/empty.map "python dist\starter_bots\python\MyBot.py" "python dist\starter_bots\python\MyBot.py" --nolaunch
+python playgame.py --verbose --fill --log_input --log_output --log_error --log_dir game_logs --turns 80 --strict --map_file maps/empty.map "python dist\starter_bots\python\MyBot.py" "python dist\starter_bots\python\MyBot.py" --nolaunch
 pause
