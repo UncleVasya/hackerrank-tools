6287011c9460b0d42a43bf3231328e6f21cf58f7
UncleVasya (Oleg Ovcharenko) <Oleg_Ovcharenko@list.ru>
2014-06-30 11:41:29 +0400

    #3 PW visualizer replaced with a GoL one

--- planetwars/example-game.cmd
+++ planetwars/example-game.cmd
@@ -1,2 +1,2 @@
-python playgame.py --verbose --fill --log_input --log_output --log_error --log_dir game_logs --strict "python dist\starter_bots\python\MyBot.py" "python dist\starter_bots\python\MyBot.py" --nolaunch
+python playgame.py --verbose --fill --log_input --log_output --log_error --log_dir game_logs --strict "python dist\starter_bots\python\MyBot.py" "python dist\starter_bots\python\MyBot.py"
 pause
