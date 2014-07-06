8249d71a00b4652ec1774ecb4a1f624a58b6bae0
UncleVasya (Oleg Ovcharenko) <Oleg_Ovcharenko@list.ru>
2014-07-06 23:57:42 +0400

    #22 bots files rearrangement

--- game-of-life/example-game.sh
+++ game-of-life/example-game.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./playgame.py --verbose --fill --log_input --log_output --log_error --log_dir game_logs --strict "dist\6_1_Game_Of_Life_[old].exe" "python dist\starter_bots\python\RandomBot.py" --turntime 5000
+./playgame.py --verbose --fill --log_input --log_output --log_error --log_dir game_logs --strict "bots\UncleVasya_1.x.exe" "python bots\sample_bots\RandomBot.py" --turntime 5000
