f22326910848553c44f6a71a70222e32797dc7fb
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2015-10-14 03:38:27 -0700

    #31 Basic frontend

--- website/apps/games/urls.py
+++ website/apps/games/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import url
-from apps.games.views import GameList
+from apps.games.views import GameList, GameDetail
+from apps.games.views import PlayerList, PlayerDetail
 
 urlpatterns = [
     url(r'^$', GameList.as_view()),
+
     url(r'^games/$', GameList.as_view()),
+    url(r'^game/(?P<slug>[-\w]+)/$', GameDetail.as_view()),
+
+    url(r'^players/$', PlayerList.as_view()),
+    url(r'^player/(?P<slug>[-\w]+)/$', PlayerDetail.as_view()),
+
 ]
--- website/apps/games/views.py
+++ website/apps/games/views.py
@@ -1,6 +1,21 @@
-from django.views.generic import ListView
-from apps.games.models import Game
+from django.views.generic import ListView, DetailView
+from apps.games.models import Game, Player
 
 
 class GameList(ListView):
     model = Game
+
+
+class GameDetail(DetailView):
+    model = Game
+    slug_field = 'slug__iexact'  # case-insensitive match
+
+
+class PlayerList(ListView):
+    model = Player
+
+
+class PlayerDetail(DetailView):
+    model = Player
+    slug_field = 'name__iexact'
+
