40ce7a8371ec7624e0158aa926565b8a5c8ae690
UncleVasya (Oleg Ovcharenko) <unclevasyaai@gmail.com>
2016-06-23 13:00:44 -0700

    #42 trying to fix wsgi

--- website/hackerrank_tools/wsgi.py
+++ website/hackerrank_tools/wsgi.py
@@ -11,7 +11,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hackerrank_tools.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "website.hackerrank_tools.settings")
 
 virtenv = os.environ.get('OPENSHIFT_PYTHON_DIR', '.') + '/virtenv/'
 virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
